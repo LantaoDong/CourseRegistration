98d201baf376fb656a868d08539e5a2010aee139
js464175 <js464175@dal.ca>
2018-02-20 13:42:33 -0400

    Added barebones XML for schedule view

--- app/src/main/java/com/example/courseregistration/ViewSchedule.java
+++ app/src/main/java/com/example/courseregistration/ViewSchedule.java
@@ -1,5 +1,12 @@
 package com.example.courseregistration;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class ViewSchedule {
+public class ViewSchedule extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.viewschedule);
+    }
 }
