083dfd7656b13a74b13d0d7ec73d56814b1a6887
LantaoDong <ln937174@dal.ca>
2018-02-21 15:42:01 -0400

    Created login activity and create activity that allow users to log in and create a new account.

--- app/src/main/java/com/example/courseregistration/MainActivity.java
+++ app/src/main/java/com/example/courseregistration/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.courseregistration;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Intent intentLogin = new Intent(this,LoginActivity.class);
+        startActivity(intentLogin);
     }
 }
