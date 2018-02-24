f28137869186219055266fbe0990dc2c212ad3f8
Haofan <haofan.hou@gmail.com>
2018-02-23 21:49:40 -0400

    trying to send and get data from Firebase

--- app/src/main/java/com/example/courseregistration/TimeTableActivity.java
+++ app/src/main/java/com/example/courseregistration/TimeTableActivity.java
@@ -2,13 +2,93 @@ package com.example.courseregistration;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class TimeTableActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+
+    private Button button2;
+    private EditText editText;
+    private ListView listView;
+
+    private ArrayList<String> arrayList = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_table);
 
+        button2 = (Button)findViewById(R.id.button2);
+        editText = (EditText)findViewById(R.id.editText);
+        listView = (ListView)findViewById(R.id.lv_TimeTable);
+
+        listView.setAdapter(adapter);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mDatabase.push().setValue(editText.getText().toString());
+
+
+            }
+        });
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+
+        mDatabase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                String string = dataSnapshot.getValue(String.class);
+
+                arrayList.add(string);
+
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                String string = dataSnapshot.getValue(String.class);
+
+                arrayList.remove(string);
+
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
