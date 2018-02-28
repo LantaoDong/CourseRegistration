40f83892cc911af2903df6cc7717305a5e2f46db
LantaoDong <ln937174@dal.ca>
2018-02-28 02:02:06 -0400

    refactored ui problems

--- app/src/main/java/com/example/courseregistration/MainActivity.java
+++ app/src/main/java/com/example/courseregistration/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.courseregistration;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//.
 public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
