3db7c166af353f27ce7516d5f57e49dc2eddab76
LantaoDong <ln937174@dal.ca>
2018-03-09 10:16:25 -0400

    created travis.yml

--- app/src/main/java/com/example/courseregistration/CourseFilter.java
+++ app/src/main/java/com/example/courseregistration/CourseFilter.java
@@ -4,10 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CourseFilter extends AppCompatActivity {
+    String[] subjects = {};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_filter);
+        
     }
 }
