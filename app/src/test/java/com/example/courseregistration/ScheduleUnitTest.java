6261dfd61a89ef64109a7e4970f0e9a91bcb54ce
js464175 <js464175@dal.ca>
2018-03-20 17:20:26 -0300

    implementing tests

--- app/src/test/java/com/example/courseregistration/ScheduleUnitTest.java
+++ app/src/test/java/com/example/courseregistration/ScheduleUnitTest.java
@@ -14,4 +14,16 @@ public class ScheduleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    //public boolean checkIfInsideTime(int startHr, int startMin, int endHr, int endMin, int newHr, int newMin) {
+
+    @Test
+    public void timeTest_isCorrect() throws Exception {
+        assertEquals(ViewSchedule.checkIfInsideTime(8,30,10,30,8,30), true);
+    }
+
+    @Test
+    public void timeTest_isCorrect2() throws Exception {
+        assertEquals(ViewSchedule.checkIfInsideTime(8,30,10,30,11,00), false);
+    }
 }
