e090ce73c5b74ee74e76e92962ac8a0fe50a47a6
dengyiran <yr451585@dal.ca>
2018-03-20 16:57:45 -0300

    fix some bug

--- app/src/main/java/com/example/courseregistration/Adddroptable.java
+++ app/src/main/java/com/example/courseregistration/Adddroptable.java
@@ -14,7 +14,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.database.*;
-  
+
 public class Adddroptable extends AppCompatActivity {
 
     @Override
