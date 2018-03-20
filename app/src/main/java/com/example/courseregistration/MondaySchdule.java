fb1712a2dd274f716576966a4954edd29c630cd8
jaspering21 <jasperingclearlove@gmail.com>
2018-03-20 01:14:50 -0300

    update course information

--- app/src/main/java/com/example/courseregistration/MondaySchdule.java
+++ app/src/main/java/com/example/courseregistration/MondaySchdule.java
@@ -1,9 +1,11 @@
 package com.example.courseregistration;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.example.courseregistration.models.MyApplicationData;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -14,25 +16,28 @@ import com.google.firebase.database.ValueEventListener;
  * Created by Jaspering21 on 2018-03-19.
  */
 
-public class MondaySchdule  extends AppCompatActivity {
+public class MondaySchdule  extends Activity {
 
     TextView courseText;
     String courseName = "";
+    MyApplicationData appstate;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_monday);
         courseText = (TextView)findViewById(R.id.courseText);
-        DatabaseReference cs = FirebaseDatabase.getInstance().getReference("users");
-        cs.addListenerForSingleValueEvent(new ValueEventListener() {
+        DatabaseReference monday = FirebaseDatabase.getInstance().getReference("users");
+        monday.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String uid = appstate.firebaseReference.push().getKey();
+                    String userkey=snapshot.getKey();
                     String name = snapshot.child("registered courses").getValue().toString();
                     //name += " " + snapshot.child("courseName").getValue().toString();
-                    if (courseName.equals(name)) {
-                        courseText.setText(name);
-                    }
+                    courseText.setText(name);
+
                 }
             }
             @Override
