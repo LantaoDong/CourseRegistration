d66afa594386f4de32574f077703e00a3cfe7780
js464175 <js464175@dal.ca>
2018-03-28 12:21:22 -0300

    adding in UI for viewing different semesters There is a bug where it crashes right now
    Justin & Jasper

--- app/src/main/java/com/example/courseregistration/UserActivity.java
+++ app/src/main/java/com/example/courseregistration/UserActivity.java
@@ -50,7 +50,7 @@ public class UserActivity extends AppCompatActivity {
         viewSchedule.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent viewSche = new Intent(UserActivity.this,ViewSchedule.class);
+                Intent viewSche = new Intent(UserActivity.this,ViewScheduleSemester.class);
                 viewSche.putExtra("userID",userID);
                 startActivity(viewSche);
             }
---
+++ app/src/main/java/com/example/courseregistration/ViewScheduleSemester.java
@@ -0,0 +1,52 @@
+package com.example.courseregistration;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+public class ViewScheduleSemester extends AppCompatActivity{
+
+
+    private ListView semestersListView;
+    ArrayAdapter semesteradapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.viewschedule);
+
+        Intent intent = getIntent();
+        final String userID = intent.getStringExtra("userID");
+
+        semestersListView = (ListView) findViewById(R.id.semesterListView);
+        ArrayList<String> semesters = new ArrayList<String>();
+        semesters.add("Fall");
+        semesters.add("Winter");
+        semesters.add("Summer");
+        semesteradapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, semesters);
+        semestersListView.setAdapter(semesteradapter);
+
+        semestersListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            // onItemClick method is called everytime a user clicks an item on the list
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Day that was clicked on by the user
+                String selectedSemester = String.valueOf(parent.getItemAtPosition(position));
+
+                Intent intent = new Intent(ViewScheduleSemester.this, ViewSchedule.class);
+                intent.putExtra("semester", selectedSemester);
+                intent.putExtra("userID", userID);
+
+                startActivity(intent);
+            }
+        });
+
+    }
+}
